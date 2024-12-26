9bbe303e207f78ef5f467e65270aaf50c8e64744
Isvaya <tasyana.pulatova@gmail.com>
2024-12-26 14:14:00 +0500

    feat: add slider

--- index.js
+++ index.js
@@ -11,3 +11,44 @@ document.addEventListener('DOMContentLoaded', function() {
     window.window.location.href = `/${selectedLanguage}/index.html`;
   });
 });
+
+
+///SLIDER SERVICES 
+
+const container = document.querySelector('.services__container--hidden');
+const boxContainer = document.querySelector('.services__box-container');
+const leftButton = document.querySelector('.left-btn');
+const rightButton = document.querySelector('.right-btn');
+const cards = document.querySelectorAll('.box-container--box');
+
+let offset = 0;
+const step = 433;
+
+const totalWidth = cards.length * step;
+const visibleWidth = container.offsetWidth;
+const maxOffset = Math.max(0, totalWidth - visibleWidth);
+
+function updateButtons() {
+  leftButton.disabled = offset === 0;
+  rightButton.disabled = Math.abs(offset) >= maxOffset;
+}
+
+rightButton.addEventListener('click', () => {
+  if (Math.abs(offset) < maxOffset) {
+    offset -= step;
+    offset = Math.max(-maxOffset, offset);
+    boxContainer.style.transform = `translateX(${offset}px)`;
+    updateButtons();
+  }
+});
+
+leftButton.addEventListener('click', () => {
+  if (offset < 0) {
+    offset += step;
+    offset = Math.min(0, offset);
+    boxContainer.style.transform = `translateX(${offset}px)`;
+    updateButtons();
+  }
+});
+
+updateButtons();
